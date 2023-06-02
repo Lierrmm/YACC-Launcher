22f1966cc67050aced1be31902788b2ef19554d8
Lierrmm <lierrmm@gmail.com>
2023-06-02 08:00:36 +0100

    feat: remove changelog link for now

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,11 @@
   import { Route } from "tinro";
   import Play from "./pages/Play.svelte";
   import Settings from "./pages/Settings.svelte";
-  import Changelog from "./pages/Changelog.svelte";
 </script>
 
 <main class="h-screen rounded-md">
+  <div id="background" />
   <Titlebar />
   <Route path="/"><Play /></Route>
   <Route path="/settings"><Settings /></Route>
-  <!-- <Route path="/changelog"><Changelog /></Route> -->
 </main>
